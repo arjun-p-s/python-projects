4e6266db8eda734d2bae6735e5c59776397c9ddc
arjun p s <hai.arjunps@gmail.com>
2025-06-18 17:31:14 +0530

    madlibs file is added

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+
+person_name = input("Person Name: ")
+prefession = input("prefession: ")
+hobby = input("hobby: ")
+
+print(f"hello everyone i am {person_name} , i am a {prefession}. i love {hobby}")
